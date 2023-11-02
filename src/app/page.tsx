1d1f5b223a98d33fe6a97d35084a4328f641c729
ozan yildirim <ozanyildirim1000@gmail.com>
2023-11-02 22:58:34 +0000

    developer tools setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Button variant="destructive">Click me</Button>
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                <Button variant="destructive">Click me</Button>
+            </div>
+        </main>
+    )
 }
