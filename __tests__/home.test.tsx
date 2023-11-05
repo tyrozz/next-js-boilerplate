2c6eacae8ea594d2af54d1164b00c5db40f99481
ozan yildirim <ozanyildirim1000@gmail.com>
2023-11-05 11:54:36 +0000

    test related updates, cypres set up

---
+++ __tests__/home.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import Home from '@/app/(marketing)/page'
+
+describe('Home', () => {
+    it('should have About Page button', () => {
+        render(<Home />)
+        // const myElem = screen.getByText(/About Page/i)
+        // expect(myElem).toBeInTheDocument()
+    })
+})
